750b1b16687835dd2625428fdcee2069a5bb8389
nagapavan <nagapavan@uber.com>
2025-10-22 10:43:40 +0530

    Revert "Merge pull request #45 from anirudhraja/fix_int64_encode"
    This reverts commit 547ab5812c7d0888a925d457a435af8d027131d7, reversing
    changes made to b75d939bd7c53430d169d295d93ef1fbc61ec1b2.

--- wire/message.go
+++ wire/message.go
@@ -3,7 +3,6 @@ package wire
 import (
 	"encoding/json"
 	"fmt"
-	"math"
 	"sort"
 	"strconv"
 
@@ -362,9 +361,17 @@ func (me *MessageEncoder) encodePrimitiveField(encoder *Encoder, value interface
 		NewBytesEncoder(encoder).EncodeBytes(v)
 		return nil
 	case schema.TypeInt32:
-		v, err := convertToInt32(value)
-		if err != nil {
-			return err
+		v, ok := value.(int32)
+		if !ok {
+			jsonVal, ok := value.(json.Number)
+			if !ok {
+				return fmt.Errorf("expected int32, got %T", value)
+			}
+			val, err := strconv.Atoi(jsonVal.String())
+			if err != nil {
+				return err
+			}
+			v = int32(val)
 		}
 		NewVarintEncoder(encoder).EncodeInt32(v)
 		return nil
@@ -722,9 +729,18 @@ func (me *MessageEncoder) encodeWrapperField(encoder *Encoder, value interface{}
 			return err
 		}
 		var val int32
-		val, err = convertToInt32(actualValue)
-		if err != nil {
-			return err
+		switch v := actualValue.(type) {
+		case int32:
+			val = v
+		case json.Number:
+			var i64 int64
+			i64, err = v.Int64()
+			if err != nil {
+				return fmt.Errorf("invalid int32: %v", err)
+			}
+			val = int32(i64)
+		default:
+			return fmt.Errorf("unexpected type for int32: %T", actualValue)
 		}
 		tag := MakeTag(FieldNumber(1), WireVarint)
 		ve.EncodeVarint(uint64(tag))
@@ -802,29 +818,6 @@ func (me *MessageEncoder) encodeWrapperField(encoder *Encoder, value interface{}
 	be := NewBytesEncoder(encoder)
 	be.EncodeBytes(wrapperEncoder.Bytes())
 	return nil
-}
-
-func convertToInt32(value interface{}) (int32, error) {
-	switch v := value.(type) {
-	case int32:
-		return v, nil
-	case int64:
-		if v < math.MinInt32 || v > math.MaxInt32 {
-			return 0, fmt.Errorf("value %d out of int32 range", v)
-		}
-		return int32(v), nil
-	case json.Number:
-		i64, err := v.Int64()
-		if err != nil {
-			return 0, fmt.Errorf("invalid int32: %v", err)
-		}
-		if i64 < math.MinInt32 || i64 > math.MaxInt32 {
-			return 0, fmt.Errorf("value %d out of int32 range", i64)
-		}
-		return int32(i64), nil
-	default:
-		return 0, fmt.Errorf("unexpected type for int32: %T", value)
-	}
 }
 
 // encodeMapField encodes a map field
